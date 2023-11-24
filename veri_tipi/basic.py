26fa570b806382dbdd17d58505d2d04ed8b6f397
batuhanDev <batuhantandkay@gmail.com>
2023-11-24 22:00:45 +0300

    Data Types

---
+++ veri_tipi/basic.py
@@ -0,0 +1,40 @@
+# tipi otomatik algılar yani
+
+####### int sayi = 5 gibi başına int yazmaya gerek yok #######
+
+##--INT--##
+sayi = 5
+print(sayi)
+
+##--FLOAT--##
+sayi2 = 5.0
+sayi3 = 3.2 # noktalı olmalı
+print(sayi2)
+print(sayi3)
+
+##-STRING--##
+metin = "Hello World!"
+print(metin)
+
+##--BOOLEAN--##
+gercek = True # 0'dan farklı her değer
+yalan = False # 0
+print(gercek)
+print(yalan)
+
+##--LIST--##
+list  = [1,True,"hello", 4.0] # değiştirilebilir liste
+print(list)
+
+##--SET--##
+set  = {1,1,1,1,2,2,False,False} # bir değerden birden çok varsa o değeri tekli yapar yani çıktısı = {False,1,2} olur.
+print(set)
+
+##--DICTIONARY--##
+dict = {'isim':'Batuhan', 'Yas ':'20'} # anahtar ve değer olarak düşünebiliriz anahtar = isim, değer = batuhan'dır.
+print(dict)
+
+##--TUPLE--##
+tup  = (1,2,True) # değiştirilemez liste
+print(tup)
+
