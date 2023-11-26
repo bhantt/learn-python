c30f69a838f0d412d513ac984b10d5168b8cd02b
batuhanDev <batuhantandkay@gmail.com>
2023-11-26 22:05:53 +0300

    Update Fonksiyonlar

--- fonksiyonlar/fonk.py
+++ fonksiyonlar/fonk.py
@@ -59,3 +59,57 @@ def buyuk_olan(a,b):
    
 print(buyuk_olan(45,80)) # output: 80
 print(buyuk_olan(30,20)) # output: 30
+
+#-----------------------------------------------#
+
+"""
+Fonksiyonların ilişkisi
+"""
+
+def yazdir(a,b):
+   buyuk = buyuk_olan(a,b)
+   metin = "{} daha buyuktur".format(buyuk)
+   print(metin)
+   
+yazdir(4,5) # output: 5 daha buyuktur
+
+#-----------------------------------------------#
+
+"""
+Fonksiyonlar birden fazla sonuc döndürebilir
+"""
+
+def isim_soyimi_ayirma(isim_soyisim):
+   isim = isim_soyisim.split()[0]
+   soyisim = isim_soyisim.split()[1]
+   return isim,soyisim
+
+print(isim_soyimi_ayirma("Ahmet Han")) # output: ('Ahmet', 'Han')
+
+#-----------------------------------------------#
+
+"""
+args ifadesi
+"""
+# def isim_soyisim_birlestir(isim,soyisim): # toplam 3 isimli birisi bu fonksiyonu kullanamaz
+#     return " ".join(isim,soyisim)
+
+
+def isim_soyisim_birlestir(*args): # içeriye kaç değer girdiğinin önemi yok.
+   return " ".join(args)
+
+print(isim_soyisim_birlestir("Ahmet", "Han", "Gundogdu")) # output: Ahmet Han Gundogdu
+
+#-----------------------------------------------#
+"""
+**kwargs ifadesi
+"""
+
+def gobek_adi_yazdir(**kwargs): # Aynı dictionary mantığı. 'gobekadi' anahtarının karşılığını yazdırır. 
+   if 'gobekadi' in kwargs:
+      print(kwargs['gobekadi'])
+   else:
+      print('gobek adi yok')
+      
+gobek_adi_yazdir(adi="Ahmet", gobekadi="Han", soyad="Gundogdu") # output: Han
+gobek_adi_yazdir(adi="Hasan", soyad = "Decrilmez") # output: gobek adi yok
