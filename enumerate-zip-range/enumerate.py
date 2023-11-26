bf3327fc9a9850d01b135ebb9b30d976cf9c24cb
batuhanDev <batuhantandkay@gmail.com>
2023-11-26 13:08:24 +0300

    enumerate-range-zip

---
+++ enumerate-zip-range/enumerate.py
@@ -0,0 +1,13 @@
+harfler = ['a','b','c','d','e']
+
+for index,harf in enumerate(harfler):
+   print(index,harf)
+   
+""" harflerin indexlerini numaralandırır.
+
+Print Output:
+0 a
+1 b
+2 c
+3 d
+4 e  """
---
+++ enumerate-zip-range/zip.py
@@ -0,0 +1,5 @@
+ulkeler = ['TR','FR','DE','GB']
+sira = range(1,5)
+
+for ulke in zip(ulkeler,sira):
+   print(ulke)
